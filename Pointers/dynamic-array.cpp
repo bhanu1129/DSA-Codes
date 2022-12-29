88939b90b552c9824750b65c1226b1f9743255bf
bhanu1129 <bhanubansal913@gmail.com>
2022-12-29 15:33:03 +0530

    dynamic array

---
+++ Pointers/dynamic-array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int getSum(int *arr, int n){
+
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum = sum + arr[i];
+        // sum = sum + *(arr+i);
+    }
+
+    return sum;
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    int *arr = new int[n];  //variable size array
+
+    //taking input of array
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int ans = getSum(arr, n);
+    cout<<ans<<endl;
+
+    return 0;
+}       
