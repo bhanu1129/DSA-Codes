5e3a47c0fa075e772fa7e0c0ae1c1c7f25731cb7
bhanu1129 <bhanubansal913@gmail.com>
2023-02-22 19:29:16 +0530

    max subarray sum of size k

---
+++ Arrays/maximum-subarray-sum-of-size-k.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    
+    int sum=0;
+
+    for(int i=0; i<k; i++){
+        sum=sum+arr[i];
+    }
+    int max = sum;
+    for(int i=k; i<n; i++){
+        sum=sum+arr[i];
+        sum=sum-arr[i-k];
+        if(sum>max) max=sum;
+    }
+
+    cout<<max<<endl;
+
+    return 0;
+}
